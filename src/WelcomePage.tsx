ef310eed0c9b2aff0d8adee2db4c1ee8a387b285
lucas <lucas@propi.dev>
2024-05-26 21:40:34 -0300

    fix: #5 renamed class to className, because React

--- src/WelcomePage.tsx
+++ src/WelcomePage.tsx
@@ -1,13 +1,13 @@
 export function WelcomePage() {
   return (
     <>
-      <div class="flex justify-end">
-        <div class="p-2 m-2"> Home </div>
-        <div class="p-2 m-2"> History </div>
-        <div class="p-2 m-2"> Chat </div>
-        <div class="p-2 m-2"> Support </div>
+      <div className="flex justify-end">
+        <div className="p-2 m-2"> Home </div>
+        <div className="p-2 m-2"> History </div>
+        <div className="p-2 m-2"> Chat </div>
+        <div className="p-2 m-2"> Support </div>
       </div>
-      <div class="p-4 m-4 flex-none justify-center">
+      <div className="p-4 m-4 flex-none justify-center">
         Welcome to yuma-ui 🚀.
         <p> A simple UI for interact with LLM </p>
         <p> Click on the menu to navigate </p>
